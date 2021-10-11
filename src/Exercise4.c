ab2ee47215d15709eb1f5a619eaf0c54bb74ba8d
Truong Duc Duy Khang <ITITIU20227@student.hcmiu.edu.vn>
2021-10-11 15:42:32 +0700

    Lab Week 2

--- src/Exercise4.c
+++ src/Exercise4.c
@@ -21,11 +21,35 @@ int main(int argc, char *argv[]) {
 		test_array[i] = atoi(argv[i+1]);
 	}
 	//Your codes here
+	int a;
 	if(n % 2 != 0) {
-		printf("%d is an odd number.", n);
+		printf("%d is an odd number. Please enter an even number.", n);
 	}
 	else {
-		
-	}	
+		for(int i = 0; i < n/2; ++i) {
+			for(int j = i + 1; j < n/2; ++j) {
+				if(test_array[i] > test_array[j]) {
+					a = test_array[i];
+					test_array[i] = test_array[j];
+					test_array[j] = a;
+				}
+			}
+		}
+		for(int i = n/2; i < n; ++i) {
+			for(int j = i + 1; j < n; ++j) {
+				if(test_array[i] < test_array[j]) {
+					a = test_array[i];
+					test_array[i] = test_array[j];
+					test_array[j] = a;
+				}
+			}
+		}
+		for(int i = 0; i < n/2; ++i) {
+			printf("%d ", test_array[i]);
+		}
+		for(int i = n/2; i < n; ++i) {
+			printf("%d ", test_array[i]);
+		}
+	}
 	return 0;
 }
